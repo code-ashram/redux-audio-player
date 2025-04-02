1d2be948437bcd262d2fc80bc07f04237eceda97
Andrei Yeutukhou <gavnorock84@hotmail.com>
2025-04-02 20:26:36 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeSwitcher } from '@/components/ui/theme-switcher'
+
+import './App.scss'
+
+function App () {
+
+  return (
+    <>
+      <ThemeSwitcher />
+    </>
+  )
+}
+
+export default App
